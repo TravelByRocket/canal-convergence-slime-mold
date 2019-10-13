efbf7ce148397536beff62c0908b5cb99120510f
Bryan Costanza <bryan.costanza@colorado.edu>
2019-10-13 09:39:13 -0600

    tool for getting MAC and IP of ESP board to register on university headless management

---
+++ Arduino/prepareEsp8266ForUcbHeadless/prepareEsp8266ForUcbHeadless.ino
@@ -0,0 +1,28 @@
+#include <ESP8266WiFi.h>
+#include <wifiucbwireless.h> // imports String vars for SSID and PASS
+
+void setup() {
+  Serial.begin(115200);
+  while(!Serial){
+    // wait for serial connection
+  }
+  Serial.println("");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(500);
+  }
+
+  Serial.println("");
+  Serial.print("Connected! IP address: ");
+  Serial.println(WiFi.localIP());
+
+  Serial.println();
+  Serial.print("MAC: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+  
+}
